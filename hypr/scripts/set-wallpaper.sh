e0ee4ba2e77c91fc6e3d157147f65f864553bfd2
Erik Tholén <ltd180@gmail.com>
2026-03-21 16:59:28 +0100

    Add .gitignore

--- hypr/scripts/set-wallpaper.sh
+++ hypr/scripts/set-wallpaper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 img=$(find ~/Pictures/Wallpapers -type f | shuf -n 1)
-swww img "$img" -t random --transition-duration 1
+swww img "$img" -t any --transition-duration 0.5 --transition-fps 240 --transition-step 255
