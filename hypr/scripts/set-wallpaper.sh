e63f9d449788759a2f78869a34ad3ec3338a9a88
Erik Tholén <ltd180@gmail.com>
2026-03-21 23:53:39 +0100

    update hyprland config and wallpaper script

--- hypr/scripts/set-wallpaper.sh
+++ hypr/scripts/set-wallpaper.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-img=$(find ~/Pictures/Wallpapers -type f | shuf -n 1)
-swww img "$img" -t any --transition-duration 0.5 --transition-fps 240 --transition-step 255
+
+WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
+
+LAST_INDEX_FILE="$HOME/.last_wallpaper_index"
+
+mapfile -t WALLPAPERS < <(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" \) | sort)
+
+NUM_WALLPAPERS=${#WALLPAPERS[@]}
+
+if [[ -f "$LAST_INDEX_FILE" ]]; then
+    INDEX=$(< "$LAST_INDEX_FILE")
+else
+    INDEX=-1
+fi
+
+INDEX=$(( (INDEX + 1) % NUM_WALLPAPERS ))
+
+swww img "${WALLPAPERS[$INDEX]}" -t grow --transition-duration 0.4 --transition-fps 240 --transition-step 255 --transition-pos center
+
+echo "$INDEX" > "$LAST_INDEX_FILE"
