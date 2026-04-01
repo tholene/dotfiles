ab6e2132a8bbfefac7b37d706ae4aa8019002058
Erik Tholén <ltd180@gmail.com>
2026-04-01 17:13:25 +0200

    Add hyprquickpaper script

--- quickshell/hyprquickpaper/commands.sh
+++ quickshell/hyprquickpaper/commands.sh
@@ -4,8 +4,11 @@ WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
 LAST_INDEX_FILE="$HOME/.last_wallpaper_index"
 SYMLINK_PATH="$WALLPAPER_DIR/ACTIVE_WALLPAPER.jpg"
 
-ln -sf "${NEW_WALLPAPER}" "$SYMLINK_PATH"
+ln -sf "$1" "$SYMLINK_PATH"
 
 awww img "${SYMLINK_PATH}" -t grow --transition-duration 0.4 --transition-fps 240 --transition-step 255 --transition-pos center
 
 echo "$INDEX" > "$LAST_INDEX_FILE"
+
+awww img $1 -t grow --transition-duration 0.4 --transition-fps 240 --transition-step 255 --transition-pos center
+
